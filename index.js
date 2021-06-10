3aa03d2fd217534e1118d87ad0c3c0ed223be1b6
MykalB <mykalb081@gmail.com>
2021-06-09 20:41:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,42 @@ function scatterPlot(dataset){
     const height = 400;
     const width = 800;
     const padding = 60;
+    
+    let timeFormat = d3.timeFormat('%M:%S');
+    const years = dataset.map((data) => {
+        console.log(typeof data.Year);
+        return data.Year;
+    });
+    const xScale = d3.scaleLinear()
+                     .domain(d3.min(years), d3.max(years))
+                     .range([padding, width - padding]);
+    
+    const times = dataset.map((data) => {
+        return data.Time;
+    });
+    var xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
+    const yScale = d3.scaleTime()
+                     .domain(d3.min(times), d3.max(times))
+                     .range([padding, height - padding]);
+    
+    var yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
+    console.log(years);
+    console.log(times);
 
     const svg = d3.select('.visContainer')
                   .append('svg')
                   .attr('width', width)
                   .attr('height', height);
+    
+    svg.selectAll('circle')
+       .data(dataset)
+       .enter()
+       .append('circle')
+       .attr('class', 'dot')
+       .attr('data-xvalue',(d) => d.Year)
+       .attr('data-yvalue',(d) => d.Time)
+       .attr('cx', (d) => xScale(d.Year))
+       .attr('cy', (d) => yScale(d.Time))
+       .attr('r', 5);
                   
 }
