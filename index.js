e331c8ca2aa17ec6344ca66a602797c508382ea8
MykalB <mykalb081@gmail.com>
2021-06-10 16:06:31 -0400

    Functionality
    - Added tooltips
    - Upgrade D3 version in use
    - Added Legend

--- index.js
+++ index.js
@@ -8,20 +8,27 @@ xhr.onload = () => {
 }
 
 function scatterPlot(dataset){
+    console.log(dataset);
+    // SVG constants.
     const height = 400;
     const width = 800;
     const padding = 60;
     
-    let timeFormat = d3.timeFormat('%M:%S');
+    const timeFormat = d3.timeFormat('%M:%S');
+    
+    const keys = ['Doping allegations', 'No doping Allegations'];
+    const colors = d3.scaleOrdinal()
+                   .domain(keys)
+                   .range(d3.schemeSet1);
 
+    // Data constants.
     const years = dataset.map((data) => data.Year);
     const times = dataset.map((data) => {
         let parsedTime = data.Time.split(':');
         return new Date(2021, 0, 1, 0, parsedTime[0], parsedTime[1]);
     });
-    console.log('years:', years);
-    console.log('times:', times);
-
+    
+    // Create x and y scales.
     const xScale = d3.scaleLinear()
                      .domain([d3.min(years) -1 , d3.max(years) + 1])
                      .range([padding, width - padding]);
@@ -30,27 +37,66 @@ function scatterPlot(dataset){
                      .domain(d3.extent(times))
                      .range([padding, height - padding]);
     
+    // Set x and y axis displays.
     var xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
     var yAxis = d3.axisLeft(yScale).tickFormat(timeFormat);
-    console.log(xScale(years[5]));
-    console.log(yScale(times[5]));
 
+    // Tooltip
+    const tooltip = d3.select('.visContainer')
+                      .append('div')
+                      .attr('id', 'tooltip')
+                      .style('opacity', 0);
+
+    // Create SVG.
     const svg = d3.select('.visContainer')
                   .append('svg')
                   .attr('width', width)
                   .attr('height', height);
     
+    // Add Title.
+    svg.append('text')
+       .attr('id', 'title')
+       .attr('x', width / 2)
+       .attr('y', 0 + padding/2)
+       .attr('text-anchor', 'middle')
+       .attr('font-size', '30px')
+       .text('Doping in professionl biking');
+    
+    // Plot data.
     svg.selectAll('circle')
        .data(dataset)
        .enter()
        .append('circle')
        .attr('class', 'dot')
        .attr('data-xvalue',(d) => d.Year)
-       .attr('data-yvalue',(d) => d.Time)
+       .attr('data-yvalue',(d, i) => times[i])
        .attr('cx', (d) => xScale(d.Year))
        .attr('cy', (d, i) => yScale(times[i]))
-       .attr('r', 5);
+       .attr('r', 5)
+       .style('fill', (d) => {
+          return colors(
+              d.Doping === '' ? keys[0] : keys[1]
+          );
+       })
+       // Implement tooltips when hover of plot point.
+       .on('mouseover', (event, data) => {
+           tooltip.transition().duration(200).style('opacity', 0.9);
+           tooltip
+                  .html(
+                    data.Name + ': ' + 
+                    data.Nationality + '<br>' + 
+                    'Year: ' + data.Year +  '<br>' +
+                    'Time: ' + data.Time
+                  )
+                  .attr('data-year', data.Year)
+                  .style('left', (event.pageX) + 'px')
+                  .style('top', (event.pageY) + 'px')
+       })
+       .on('mouseout', () => {
+           tooltip.transition().duration(200).style('opacity', 0);
+       });
     
+    // Display x and y 
     svg.append('g')
        .attr('id', 'x-axis')
        .attr('transform', 'translate(0,' + (height - padding) + ')')
@@ -60,4 +106,26 @@ function scatterPlot(dataset){
        .attr('id', 'y-axis')
        .attr('transform', 'translate(' + padding + ', 0)')
        .call(yAxis);
+    
+    // Legend
+    const legendContainer = svg.append('g').attr('id', 'legend');
+    legendContainer.selectAll('rect')
+                   .data(keys)
+                   .enter()
+                   .append('rect')
+                   .attr('x', width - padding * 3)
+                   .attr('y', (d, i) => 20 * i + 100)
+                   .attr('width', 10)
+                   .attr('height', 10)
+                   .style('fill', (d) => colors(d));
+    legendContainer.selectAll('text')
+          .data(keys)
+          .enter()
+          .append('text')
+          .attr('x', width - (padding * 3) + 15)
+          .attr('y', (d, i) => 20 * i + 105)
+          .style('fill', (d) => colors(d))
+          .text((d) => d)
+          .attr('text-anchor', 'left')
+          .style('alignment-baseline', 'middle');
 };
